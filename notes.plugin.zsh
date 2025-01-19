0fd9ea69c1979f5280b439643bf1a77d434f0b49
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2025-01-19 12:55:39 +0100

    feat: The notes are now in Markdown format

--- notes.plugin.zsh
+++ notes.plugin.zsh
@@ -2,7 +2,7 @@ mkdir ~/.notes 2> /dev/null
 
 # Create of modify an existing note
 function note() {
-    $EDITOR ~/.notes/$1
+    $EDITOR ~/.notes/$1.md
 }
 
 # List all the notes in the system
@@ -12,5 +12,5 @@ function notes() {
 
 # Delete an existing note
 function delnote() {
-    rm ~/.notes/$1
+    rm ~/.notes/$1.md
 }
